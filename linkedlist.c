cc5ad23c44350bd331b609f970fdde13c5b967e6
groundedpysics <groundedpysics@proton.me>
2023-11-29 09:28:01 +0330

    LinkedList

---
+++ linkedlist.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+void insert(struct node **head, int data) {
+    struct node *new_node = (struct node*) malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+void print_list(struct node *head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    struct node *head = NULL;
+    insert(&head, 1);
+    insert(&head, 2);
+    insert(&head, 3);
+    print_list(head);
+    return 0;
+}
+
